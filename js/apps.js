160c214ab78fa625b71efad56ca2e4e54828a0ac
cullenk <45953462+cullenk@users.noreply.github.com>
2020-01-09 18:01:05 -0500

    brian's copy

--- js/apps.js
+++ js/apps.js
@@ -81,27 +81,25 @@ function getRandomPhraseAsArray(arr){
 
 //Function to take that new string of characters and display them to the screen
 
-function addPhraseToDisplay(arr){
+function addPhraseToDisplay(arr) {
   for (let i = 0; i < arr.length; i += 1) {
-      const listItem = document.createElement('li'); // Cycles through the array of returned characters, turns them into list items.
-      listItem.textContent = arr[i]; // Assigns those list items the text content value of the original characters
-      const ul = phrase.firstElementChild; // Targets the first child element of the ".phrase" class (which is the ul) and assigns it a value of ul.
-      ul.appendChild(listItem); // adds a new child element to that ul, which is the list item we created with the stored phrase letter values
-
-
-      const alphabet = /^[A-Za-z]+$/; // Matches only alphabetic characters
-
-  if (arr[i] !== ' '){
-    listItem.classList.add('letter'); // If the new item is not a space (which makes it a letter), give it the styling from ".letter".
-  } if (!arr[i].match(alphabet)) {
-        listItem.classList.add('show'); // If it's not a letter, then automatically display it in the phrase
-  } else {
-      listItem.style.width = '3em'; // Or if it's an actual space, widen the space so we can visually tell the different phrase words apart.
+    const listItem = document.createElement('li'); // Cycles through the array of returned characters, turns them into list items.
+    listItem.textContent = arr[i]; // Assigns those list items the text content value of the original characters
+    const ul = phrase.firstElementChild; // Targets the first child element of the ".phrase" class (which is the ul) and assigns it a value of ul.
+    ul.appendChild(listItem); // adds a new child element to that ul, which is the list item we created with the stored phrase letter values
+​
+    const alphabet = /^[A-Za-z]+$/; // Matches only alphabetic characters
+​
+    if (arr[i] !== ' ') {
+      listItem.classList.add('letter'); // If the new item is not a space (which makes it a letter), give it the styling from ".letter".
+      if (!arr[i].match(alphabet)) {
+        listItem.classList.add('show'); // If it's not a letter then automatically in the phrase
+      }
+    } else {
+      listItem.style.width = '2em'; // Or if it's an actual space, widen the space so we can visually tell the different phrase words apart.
     }
   }
 }
-
-
 //Call the function to produce the new string of characters using "phrases" as the parameter, store it into the phraseArray constant. Then call the second function to display that value to the screen
 const phraseArray = getRandomPhraseAsArray(phrases);
 addPhraseToDisplay(phraseArray);
